6b12e1e24db6a9cfe6fa6f501a1601962949f981
klazify <an_email>
2022-02-08 11:27:01 -0600

    added theme example (dark mode)

--- showcase/Themes.js
+++ showcase/Themes.js
@@ -17,15 +17,20 @@ export default function Themes({onChange}) {
         <Text style={css('h1')}>
             {'Theming'}
         </Text>
-        <View style={css('row')}>
-            <Btn style={css('btn-light mr-1 flex')}
-                 textStyle={css('text text-dark')}
-                 label={'Light'}
-                 onPress={() => setTheme('default')}/>
-            <Btn style={css('btn-dark flex')}
-                 textStyle={css("text-white")}
-                 label={'Dark'}
-                 onPress={() => setTheme('custom')}/>
+        <View style={css('p-1 bg-panel')}>
+            <Text style={css('text')}>
+                {'By default, Klazify has a theme with predefined colors, but you can create your own theme, we added a Dark theme example for learn purposes.'}
+            </Text>
+            <View style={css('row mt-1')}>
+                <Btn style={css('btn-light mr-1 flex')}
+                     textStyle={css('text text-dark')}
+                     label={'Light'}
+                     onPress={() => setTheme('default')}/>
+                <Btn style={css('btn-dark flex')}
+                     textStyle={css('text-white')}
+                     label={'Dark'}
+                     onPress={() => setTheme('custom')}/>
+            </View>
         </View>
     </View>;
 }
