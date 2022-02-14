05665a973f35193dffd7d3b29a4e225493815d0a
klazify <an_email>
2022-02-13 21:37:46 -0600

    fixed badges

--- showcase/Badges.js
+++ showcase/Badges.js
@@ -10,85 +10,85 @@ export default function Badges() {
         <View style={css('bg-panel p-1 rounded-3')}>
 
             <View style={css('row flex-wrap mb-1')}>
-                <View style={css('badge bg-blue mr-1 mb-1')}>
+                <View style={css('badge-blue mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-indigo mr-1 mb-1')}>
+                <View style={css('badge-indigo mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-purple mr-1 mb-1')}>
+                <View style={css('badge-purple mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-pink mr-1 mb-1')}>
+                <View style={css('badge-pink mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-red mr-1 mb-1')}>
+                <View style={css('badge-red mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-orange mr-1 mb-1')}>
+                <View style={css('badge-orange mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-yellow mr-1 mb-1')}>
+                <View style={css('badge-yellow mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-green mr-1 mb-1')}>
+                <View style={css('badge-green mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-teal mr-1 mb-1')}>
+                <View style={css('badge-teal mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-cyan mr-1 mb-1')}>
+                <View style={css('badge-cyan mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray mr-1 mb-1')}>
+                <View style={css('badge-gray mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray-1 mr-1 mb-1')}>
+                <View style={css('badge-gray-1 mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray-2 mr-1 mb-1')}>
+                <View style={css('badge-gray-2 mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray-3 mr-1 mb-1')}>
+                <View style={css('badge-gray-3 mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray-4 mr-1 mb-1')}>
+                <View style={css('badge-gray-4 mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray-5 mr-1 mb-1')}>
+                <View style={css('badge-gray-5 mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-gray-dark mr-1 mb-1')}>
+                <View style={css('badge-gray-dark mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-primary mr-1 mb-1')}>
+                <View style={css('badge-primary mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-secondary mr-1 mb-1')}>
+                <View style={css('badge-secondary mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-success mr-1 mb-1')}>
+                <View style={css('badge-success mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-info mr-1 mb-1')}>
+                <View style={css('badge-info mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-warning mr-1 mb-1')}>
+                <View style={css('badge-warning mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-danger mr-1 mb-1')}>
+                <View style={css('badge-danger mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-white mr-1 mb-1')}>
+                <View style={css('badge-white mr-1 mb-1')}>
                     <Text style={css('text text-black text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-light mr-1 mb-1')}>
+                <View style={css('badge-light mr-1 mb-1')}>
                     <Text style={css('text text-black text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-dark mr-1 mb-1')}>
+                <View style={css('badge-dark mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
-                <View style={css('badge bg-black mr-1 mb-1')}>
+                <View style={css('badge-black mr-1 mb-1')}>
                     <Text style={css('text text-white text-bold')}>{'27'}</Text>
                 </View>
             </View>
@@ -97,7 +97,7 @@ export default function Badges() {
                 <Text style={css('text align-self-center')}>
                     {'Some Option 1'}
                 </Text>
-                <View style={css('badge bg-primary')}>
+                <View style={css('badge-primary')}>
                     <Text style={css('text text-white text-bold')}>
                         {'14'}
                     </Text>
@@ -107,22 +107,37 @@ export default function Badges() {
                 <Text style={css('text align-self-center')}>
                     {'Some Option 2'}
                 </Text>
-                <View style={css('badge bg-purple')}>
+                <View style={css('badge-purple')}>
                     <Text style={css('text text-white text-bold')}>
                         {'14'}
                     </Text>
                 </View>
             </View>
 
-            <View style={css('btn-primary mb-1 inline')}>
-                <Text style={css('text-white')}>
-                    {'Some Button'}
-                </Text>
-                <View style={css('badge badge-fixed badge-sm bg-danger')}>
-                    <Text style={css('text text-white text-bold')}>
-                        {'99+'}
+            <View style={css('row')}>
+
+                <View style={css('btn-primary mb-1 inline')}>
+                    <Text style={css('text-white')}>
+                        {'Some Button'}
                     </Text>
+                    <View style={css('badge badge-fixed badge-sm bg-danger')}>
+                        <Text style={css('text text-white text-bold')}>
+                            {'99+'}
+                        </Text>
+                    </View>
                 </View>
+
+                <View style={css('btn-dark mb-1 ml-2 inline row align-items-center rounded')}>
+                    <Text style={css('text-white align-self-center mr-1')}>
+                        {'Some Chip'}
+                    </Text>
+                    <View style={css('badge badge-sm bg-light')}>
+                        <Text style={css('text text-primary text-bold')}>
+                            {'99+'}
+                        </Text>
+                    </View>
+                </View>
+
             </View>
 
         </View>
