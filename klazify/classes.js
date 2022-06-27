c9d5eb23e519283c1b8e8d029533e4fdb6d4f41c
klazify <an_email>
2022-06-26 20:55:52 -0500

    Next: Transform?

--- klazify/classes.js
+++ klazify/classes.js
@@ -16,7 +16,7 @@ import {typography} from './modules/typography';
  */
 
 /**
- *  React Native only supports the next properties:
+ *  React Native Stylesheet only supports a limited list of properties, so the classes are limited to the list below:
  *  ["alignContent","alignItems","alignSelf","aspectRatio","backfaceVisibility","backgroundColor","borderBottomColor","borderBottomEndRadius","borderBottomLeftRadius","borderBottomRightRadius","borderBottomStartRadius","borderBottomWidth","borderColor","borderEndColor","borderEndWidth","borderLeftColor","borderLeftWidth","borderRadius","borderRightColor","borderRightWidth","borderStartColor","borderStartWidth","borderStyle","borderTopColor","borderTopEndRadius","borderTopLeftRadius","borderTopRightRadius","borderTopStartRadius","borderTopWidth","borderWidth","bottom","color","decomposedMatrix","direction","display","elevation","end","flex","flexBasis","flexDirection","flexGrow","flexShrink","flexWrap","fontFamily","fontSize","fontStyle","fontVariant","fontWeight","height","includeFontPadding","justifyContent","left","letterSpacing","lineHeight","margin","marginBottom","marginEnd","marginHorizontal","marginLeft","marginRight","marginStart","marginTop","marginVertical","maxHeight","maxWidth","minHeight","minWidth","opacity","overflow","overlayColor","padding","paddingBottom","paddingEnd","paddingHorizontal","paddingLeft","paddingRight","paddingStart","paddingTop","paddingVertical","position","resizeMode","right","rotation","scaleX","scaleY","shadowColor","shadowOffset","shadowOpacity","shadowRadius","start","textAlign","textAlignVertical","textDecorationColor","textDecorationLine","textDecorationStyle","textShadowColor","textShadowOffset","textShadowRadius","textTransform","tintColor","top","transform","transformMatrix","translateX","translateY","width","writingDirection","zIndex"]
  */
 
@@ -47,7 +47,6 @@ const defaultClasses = () => ({
     shadowOffset: {height: 4, width: 0},
     elevation: 4,
   },
-
 
   'input-active': {borderWidth: 1, borderColor: '$primary'},
   'input-error': {borderWidth: 1, borderColor: '$danger'},
