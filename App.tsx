48614c347217da6fb5c05428c9fe2b042d70576e
klazify <an_email>
2022-06-26 21:58:34 -0500

    OK

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import {useEffect, useState} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
 /**
  * You can use init() method directly,
  * we are adding an alias as example, you can use an alias when another init() method conflicts with Klazify init method.
  */
-import {color, css, init as klazify} from 'klazify';
+import {css, init as klazify, value} from 'klazify';
 
 import {StatusBar} from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
@@ -33,24 +33,25 @@ export default function App() {
   }
   
   return (
-    <ScrollView style={css('flex bg-body')}
-                contentContainerStyle={css('flex-grow pt-8')}>
+    <ScrollView style={css('flex')}
+                contentContainerStyle={css('flex-grow pt-8 bg-purple-900')}>
       <StatusBar style="auto"
-                 backgroundColor={color('$sky800').hex()}/>
+                 backgroundColor={value('$sky800')}/>
       <View style={css('row')}>
         <View style={css('flex-1 h-2 bg-green-500')}/>
         <View style={css('flex-1 h-2 bg-yellow-500')}/>
         <View style={css('flex-1 h-2 bg-red-500')}/>
         <View style={css('flex-1 h-2 bg-blue-500')}/>
       </View>
-      <View style={css('bg-purple-600 p-4 shadow')}>
+      <View style={css('bg-purple-700 p-4 shadow')}>
         <Text style={css('text text-md text-bold text-center text-white')}>
           Klazify is a Style Sheet utility
         </Text>
       </View>
-      <View style={css('p-4 mt-4')}>
+      <View style={css('flex-1 p-4 pt-8 bg-white')}>
         <Text style={css('text text-justify')}>
-          Klazify reuse styles just like CSS classes, highly inspired in tailwindcss, but with
+          Klazify reuse styles just like CSS classes, highly inspired
+          in <Text style={css('text-cyan-600 text-bold')}>Tailwindcss</Text>, but with
           the React Native limitations / features. It gives a profesional and consistent look and feel, it also can
           improve the time and the way you develop with React Native.
         </Text>
@@ -61,7 +62,7 @@ export default function App() {
         <View style={css('flex-row items-center mt-4 alert-warning p-4 bg-yellow-50 b-1 border-yellow-300 rounded-lg')}>
           <MaterialCommunityIcons name={'alert'}
                                   style={css('icon icon-md text-yellow-500 center mr-4')}/>
-          <Text style={css('flex text text-justify text-yellow-600')}>
+          <Text style={css('flex-1 text text-justify text-yellow-600')}>
             {'Klazify is NOT a component library, you can build components just by combining, creating or overriding the provided classes.'}
           </Text>
         </View>
--- default.theme.js
+++ default.theme.js
@@ -2,24 +2,23 @@ import {color, value} from 'klazify';
 import defaultClasses from './klazify/classes';
 
 export default {
-    values: {
-        defaultFontFamily: 'Roboto_400Regular',
-    },
-    extraGlobalVars: {
-        $rem: 16,
-        '$blue-custom': '#37a1e3',
-    },
-    // customClasses: () => {
-    //     return {
-    //         'primary-lighten-50': {
-    //             backgroundColor: color('$primary').lighten(50).hex(),
-    //         },
-    //         'custom-font': {
-    //             color: () => value('$black'),
-    //             fontFamily: 'PressStart2P_400Regular',
-    //         },
-    //     };
-    // },
-    customClasses: defaultClasses,
-    logs: true,
+  values: {
+    defaultFontFamily: 'Roboto_400Regular',
+  },
+  extraGlobalVars: {
+    '$blue-custom': '#37a1e3',
+  },
+  // customClasses: defaultClasses,
+  customClasses: () => {
+    return {
+      'primary-lighten-50': {
+        backgroundColor: color('$primary').lighten(50).hex(),
+      },
+      'custom-font': {
+        color: () => value('$black'),
+        fontFamily: 'PressStart2P_400Regular',
+      },
+    };
+  },
+  logs: true,
 }
